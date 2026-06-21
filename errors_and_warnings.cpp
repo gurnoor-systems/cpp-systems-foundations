1c96b44a92859fba6042c38018e376f45206d737
Gurnoor Singh <gurnoor.capital@gmail.com>
2026-06-21 17:00:19 +0530

    understanding error

---
+++ errors_and_warnings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    /* compile time error
+    std::cout << "Hello, World!" << std::endl
+    ________________________________________________________________
+
+    Run time error
+    int value = 7/10;
+    std::cout<<" Value: " << value << std::endl; */
+
+    return 0;
+}
